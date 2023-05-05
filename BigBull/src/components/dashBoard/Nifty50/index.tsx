23a2633f15720659e7578f45350a8b2714445a14
UdayPandit01 <uday.pandit@netsolutions.com>
2023-05-05 11:56:46 +0530

    feat: nse module

--- BigBull/src/components/dashBoard/Nifty50/index.tsx
+++ BigBull/src/components/dashBoard/Nifty50/index.tsx
@@ -1,12 +1,60 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import NseModule from '../../../NseModule';
+import {NseData} from '../Home/nsedata';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const Nifty50 = () => {
+  const [nseData, setNseData] = useState<NseData>();
+  useEffect(() => {
+    {
+      NseModule.onBridge('Init');
+      setTimeout(() => {
+        getMovies();
+      }, 1000);
+    }
+  }, []);
+
+  // const onClick = () => {
+  //   getMovies();
+  // };
+  const callbackSuccess = scanResult => {
+    setNseData(JSON.parse(scanResult));
+    console.log('losinggggJAVADDffd ' + scanResult);
+  };
+  const callbackError = error => {
+    console.log('errorlosinggggJAVADDffd' + error);
+  };
+  const getMovies = async () => {
+    try {
+      {
+        // NseModule.onBridge(
+        //   'https://www.nseindia.com/api/equity-stockIndices?index=NIFTY 50',
+        // );
+        NseModule.getAPIResponse(
+          'https://www.nseindia.com/api/equity-stockIndices?index=NIFTY 50',
+          callbackSuccess,
+          callbackError,
+        );
+      }
+    } catch (error) {
+      console.error('testf ' + error);
+    } finally {
+    }
+  };
+
   return (
     <View>
       <Text>Nifty50</Text>
+      {/* {nseData && <Text>{nseData.name}</Text>} */}
+      <TouchableOpacity onPress={() => onClick()}>
+        <Text>{'Go To Next'}</Text>
+        {nseData && <Text>{nseData.name}</Text>}
+        {nseData && <Text>{nseData.timestamp}</Text>}
+        {nseData && <Text>{nseData.metadata.last}</Text>}
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default Nifty50
+export default Nifty50;
